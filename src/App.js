d769fb7d206634c0176f98e715752a8b57e7f1b8
prithvirajktagadinamani@gmail.com <prithvirajktagadinamani@gmail.com>
2022-06-20 15:40:22 +0530

    PP comitted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import { BrowserRouter, Route, Router } from "react-router-dom";
-import Home from './Components/User/Home/Home';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/User/Home/Home";
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     <Router>
-       <Route exact path="/:hotel/:floor/:condition/:table" component={Home} />
-       <Route exact path="/" component={Home} />
-
-     </Router>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/:hotel/:floor/:condition/:table"
+            element={<Home />}
+          />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/User/Home/Home.jsx
+++ src/Components/User/Home/Home.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
+import { useRecoilState, useRecoilValue } from "recoil"
+import {foodItems} from "../../Atom/Atom"
+
 function Home() {
+    const {item, setItem} = useState("")
+    const [_, setFoodItems] = useRecoilState(foodItems)
+
+    const getItemsList = useRecoilValue(foodItems)
+
+    const [id, setId] = useState("")
+    const handleChange =(e) => {
+        setItem({...item, [e.target.name]:e.target.value})
+    }
+    const addItem = () => {
+        if(item){
+            setFoodItems((oldItems) => [
+                ...oldItems,
+                {
+                    id:id,
+                    text:item,
+                    icComplete: false
+                }
+            ])
+
+        }
+        console.log(getItemsList)
+    }
+    const addId =(e)=> {
+        setId(e.target.value)
+    }
+
+
+
+    const DisplayItem = (item ) => {
+        return 
+       { <div className="row">
+            <p>{item.id} {item.text}</p>
+        </div>}
+    }
   return (
     <div>
         
-        Home
+        <div className="container">
+            <div className="row">
+                <h1>HOME</h1>
+            </div>
+            <div className="row">
+                <input type="text" onChange={(e) => addId(e)} />
+                <input name="item1" value={item} type="text" onChange={(e) => handleChange(e)} />
+                <button onClick={addItem}>Add Item</button>
+            </div>
+            {/* <div className="row">
+                {getItemsList.length>0 && (
+                    <div className="row">
+                        {
+                            getItemsList.map(item => {
+                                <DisplayItem key={item.id} item={item} />
+                            })
+                        }
+                    </div>
+  )}
+            </div> */}
+        </div>
 
     </div>
   )
